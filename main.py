d9f12bb39313846c57dcb1193596f1a8144249b3
Zackary Schreiner <39150651+Zacks7348@users.noreply.github.com>
2020-03-27 20:04:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     bot.load_extension("commands")
     bot.load_extension("listeners")
 
-    bot.run("NjkzMjIzMjcwNjY4OTU5NzQ0.Xn58qA.qfC3YkOF60-7ULc9LAbrGioIGWc")
+    bot.run("BOT-TOKEN-HERE")
 
 
 
